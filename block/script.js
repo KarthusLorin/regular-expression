d3d1ce0b582dab6be1a4e14f60e605df3b7a693c
KarthusLorin <karthuslorin@gmail.com>
2018-07-08 23:58:36 +0800

    add block demo

---
+++ block/script.js
@@ -0,0 +1,15 @@
+let text = 'ByronsperByrCasper'
+
+let reg = /Byr(on|Ca)sper/g		// 匹配Byronsper或ByrCasper
+
+let result = text.replace(reg, 'X')		// XX
+
+console.log(result)
+
+let text2 = '2015-12-25'
+
+let reg2 = /(\d{4})-(\d{2})-(\d{2})/g
+
+let result2 = text2.replace(reg2, '$2/$3/$1')
+
+console.log(result2)
